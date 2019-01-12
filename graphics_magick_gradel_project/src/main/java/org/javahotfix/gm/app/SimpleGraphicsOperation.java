b23e1a43d48f6887757f1b0e3a31cdbaeee49629
Anil Sable <anilsablepatil@gmail.com>
2019-01-12 16:00:10 +0530

    Adding Support for Stream based input to Graphics Magick

--- graphics_magick_gradel_project/src/main/java/org/javahotfix/gm/app/SimpleGraphicsOperation.java
+++ graphics_magick_gradel_project/src/main/java/org/javahotfix/gm/app/SimpleGraphicsOperation.java
@@ -3,26 +3,44 @@ package org.javahotfix.gm.app;
 import org.im4java.core.ConvertCmd;
 import org.im4java.core.IM4JavaException;
 import org.im4java.core.IMOperation;
+import org.im4java.process.Pipe;
 import org.im4java.process.ProcessStarter;
 
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 public class SimpleGraphicsOperation {
 
     public static void main(String args[]) {
 
+    	String originalFilePath = "K:\\testfiles\\images\\happybirthday.jpg";
+    	
         // Lets Resize Image to 200x200 Resolution for the use as a thumbnail
-        String originalFilePath = "K:\\testfiles\\images\\happybirthday.jpg";
-        String targetFilePath = "K:\\testfiles\\images\\happybirthday_thumb.jpg";
+        String targetFilePath1 = "K:\\testfiles\\images\\happybirthday_thumb.jpg";
         SimpleGraphicsOperation obj1 = new SimpleGraphicsOperation();
         try {
-            obj1.resizeImage(originalFilePath, targetFilePath, 200, 200);
+            obj1.resizeImage(originalFilePath, targetFilePath1, 200, 200);
+            System.out.println("Success Generating thumbnail of size 200x200.");
         } catch (Exception ex) {
             System.out.println("Unable to Generate the thumbnail. Got Error :" + ex.getMessage());
             ex.printStackTrace();
         }
 
+        // Let Resize Image to 150x150 px and in PNG format	
+        String targetFilePath2 = "K:\\testfiles\\images\\happybirthday_thumb_PNG.png";
+        try {
+        	FileInputStream fis = new FileInputStream(originalFilePath);
+        	FileOutputStream fos = new FileOutputStream(targetFilePath2);
+        	obj1.resizeImage(fis, fos, 150, 150, "png");
+        	System.out.println("Success Generating thumbnail of size 150x150");
+
+        } catch (Exception ex) {
+        	System.out.println("Unable to Generate the thumbnail. Got Error :" + ex.getMessage());
+        	ex.printStackTrace();
+        }
 
     }
 
@@ -30,7 +48,11 @@ public class SimpleGraphicsOperation {
        // ProcessStarter.setGlobalSearchPath();
     }
 
-    public void resizeImage(String originalFile, String targetFile, int width, int height) throws InterruptedException, IOException, IM4JavaException {
+    public void resizeImage(
+    		String originalFile, 
+    		String targetFile, 
+    		int width, 
+    		int height) throws InterruptedException, IOException, IM4JavaException {
         ConvertCmd command = new ConvertCmd(true);
         IMOperation operation = new IMOperation();
 
@@ -40,4 +62,31 @@ public class SimpleGraphicsOperation {
 
         command.run(operation);
     }
+    
+    public void resizeImage(
+    		InputStream input, 
+    		OutputStream output, 
+    		int width, 
+    		int height,
+    		String format) throws IOException, InterruptedException, IM4JavaException {
+    	
+    	ConvertCmd command = new ConvertCmd(true);
+    	Pipe pipeIn = new Pipe(input, null); 
+    	Pipe pipeOut = new Pipe(null, output);
+    	
+    	command.setInputProvider(pipeIn);
+    	command.setOutputConsumer(pipeOut);
+    	
+    	IMOperation operation = new IMOperation();
+    	operation.addImage("-");
+    	operation.resize(width, height);
+    	operation.addImage(format + ":-");
+    	
+    	command.run(operation);
+    }
 }
+
+
+
+
+
